96b432ec2bde0976a903d088977a22bf0c15f6e0
ignaciojuarez07-max <ignaciojuarez07@gmail.com>
2026-05-28 23:12:35 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,12 +3,14 @@
 
 $host = 'aws-0-us-east-1.pooler.supabase.com'; 
 $dbname = 'postgres'; 
+// ¡ESTA ES LA LÍNEA CLAVE! El usuario completo:
 $username = 'postgres.koltwkcxjijbqvctovnn'; 
 $password = 'BioTikal2026'; 
 $port = '6543'; 
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+    // Agregamos sslmode=require por seguridad extra en la nube
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $pdo = new PDO($dsn, $username, $password);
     
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
