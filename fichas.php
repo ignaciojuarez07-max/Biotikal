01ab0814fc8b8de566259eee4148e5cf22847b36
ignaciojuarez07-max <ignaciojuarez07@gmail.com>
2026-05-29 00:17:37 -0600

    Update fichas.php

--- fichas.php
+++ fichas.php
@@ -13,7 +13,7 @@ $especies = $stmt->fetchAll();
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BioTikal | <?php echo htmlspecialchars($sub); ?></title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
